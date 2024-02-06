0448dc215a7329fd8dd895a93c08e68e2fc5f8c7
Jcaob <jcaobcatalan943@gmail.com>
2024-02-06 17:08:25 -0500

    first Draft

---
+++ app/app.js
@@ -0,0 +1,21 @@
+function openTab(evt, Tab) {
+  var i, tabcontent, tablinks;
+
+  // Hide all tab contents
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove "active" class from all tab links
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  // Display the selected tab content
+  document.getElementById(Tab).style.display = "block";
+
+  // Add "active" class to the clicked tab link
+  evt.currentTarget.classList.add("active");
+}
